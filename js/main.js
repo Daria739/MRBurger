c0d03d865dfe508784e67a28545140b1c6e208b3
Daria739 <juicebox739@gmail.com>
2019-02-11 01:17:15 +0300

    menu

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Всплывающее меню //
+
+function openMenu() {
+    const openBtn = document.querySelector('.menu-burger');
+    const menu = document.querySelector('.menu-burger-mobile');
+    const closeBtn = document.querySelector('.menu-burger-mobile-close');
+    let opacity = 0.2;
+
+    openBtn.addEventListener('click', function() {
+       menu.classList.add('menu-burger-mobile_active');
+       menu.style.opacity = 0;
+
+       setTimeout(function foo() {
+           if (opacity < 1) {
+               opacity += 0.2;
+               menu.style.opacity = opacity;
+               setTimeout (foo, 80);
+           }
+       }, 80);
+
+    });
+
+    closeBtn.addEventListener('click', function() {
+        setTimeout (function fooC(){
+            if (opacity > 0) {
+                opacity -= 0.2;
+                menu.style.opacity = opacity;
+                setTimeout (fooC, 80);
+            } else {
+                
+            menu.classList.remove('menu-burger-mobile_active');
+            }
+        }, 80);
+
+
+    });
+
+    document.addEventListener('keydown', function(e) {
+
+        /*if(e.keyCode === 27) {
+            menu.classList.remove('menu-burger-mobile_active');
+        }*/
+        setTimeout (function fooC() {
+        if(e.keyCode === 27) {
+            opacity -= 0.2;
+            menu.style.opacity = opacity;
+            setTimeout (fooC, 80);
+        } else {
+            menu.classList.remove('menu-burger-mobile_active');
+        }
+    }, 80);
+
+    });
+}
+
+openMenu()
