4407f8fb1841ea32fc9f4f3a7ab352754c0cc9b0
Daria739 <juicebox739@gmail.com>
2019-02-24 22:05:26 +0300

    one-page-scroll + mobile version

--- js/main.js
+++ js/main.js
@@ -320,6 +320,9 @@ const sections = $('.section');
 const display = $('.maincontent');
 let inScroll = false;
 
+var md = new MobileDetect(window.navigator.userAgent);
+const idMobile = md.mobile();
+
 const switchActiveClassInSideMenu = menuItemIndex => {
     $('.points__item').eq(menuItemIndex).addClass('active')
     .siblings().removeClass('active');
@@ -401,6 +404,10 @@ $('.wrapper').on('wheel', e =>{
 
 });
 
+$('.wrapper').on.('touchmove', e => {
+    e.preventDefault();
+});
+
 $(document).on('keydown', e => {
     switch(e.keyCode) {
         case 38: scrollToSection('prev'); break;
@@ -418,4 +425,16 @@ $('[data-scroll-to]').on('click', e => {
   
   });
 
+
+  if(isMobile) {
+    $(window).swipe({
+        swipe: function(event, direction) {
+            const nextOrPrev = direction === 'up' ? 'next' : 'prev';
+          scrollToSection(nextOrPrev);
+        }
+    });
+  
+  }
+
+ 
 
