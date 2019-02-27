26bb1f64e8afec3336c9fa8cd7c2ca3ee37f1b5d
Daria739 <juicebox739@gmail.com>
2019-02-27 16:51:41 +0300

    map

--- js/main.js
+++ js/main.js
@@ -245,7 +245,7 @@ const clearBtn = myForm.querySelector('.js-btn-reset');
 
 
 myForm.addEventListener('submit', e=>{
-    event.preventDefault();
+    e.preventDefault();
 
     if (validateForm(myForm)) {
         const name = myForm.elements.name.value;
@@ -543,27 +543,92 @@ $('.player__splash').on('click', e => {
 
 
   ymaps.ready(init);
- 
+
+
+  var placemarks = [
+    {
+        latitude: 59.97,
+        longitude: 30.31,
+        hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>',
+        balloonContent: [
+            '<div class="map__balloon></div>',
+            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+            'Самые вкусные бургеры у нас! Заходите по адресу: ул. Литераторов, д. 19',
+            '</div>'
+        ]
+
+    },
+
+
+    {
+        latitude: 59.94,
+        longitude: 30.25,
+        hintContent: '<div class="map__hint">Малый проспект В О, д 64</div>',
+        balloonContent: [
+            '<div class="map__balloon></div>',
+            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+            'Самые вкусные бургеры у нас! Заходите по адресу: Малый проспект В О, д 64',
+            '</div>'
+        ]
+
+    },
+
+    {
+        latitude: 59.93,
+        longitude: 30.34,
+        hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>',
+        balloonContent: [
+            '<div class="map__balloon></div>',
+            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+            'Самые вкусные бургеры у нас! Заходите по адресу: наб. реки Фонтанки, д. 56',
+            '</div>'
+        ]
+
+    }
+
+
+  ],
+
+geoObjects = [];
+
 function init(){     
  
-   var myMap = new ymaps.Map("map", {
+   var map = new ymaps.Map("map", {
         center: [59.94, 30.32],
         zoom: 12,
         controls: ['zoomControl'],
         behaviors: ['drag']
     });
 
-    var placemark = new ymaps.Placemark([59.97, 30.31], {
-        hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>'
-
-    },
-
-    {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/map-marker.png',
-        iconImageSize: [46, 57]
-    });
+    for (var i = 0; i < placemarks.length; i++) {
+        geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude], {
+            hintContent: placemarks[i].hintContent,
+            balloonContent: placemarks[i].balloonContent.join('')
     
-    map.geoObjects.add(placemark); 
- 
+        },
+    
+        {
+            iconLayout: 'default#image',
+            iconImageHref: './img/map-marker.png',
+            iconImageSize: [46, 57],
+            iconImageOffset: [-23, -57]
+        });
+
+        
+
+        var clusterer = new ymaps.Clusterer({
+
+        });
+        map.geoObjects.add(clusterer); 
+        //map.geoObjects.add(placemark); 
+        clusterer.add(geoObjects);
+    }
+
+
+
+    
+
+  
+    
+   
 }
